82e78e2eb4d72444fd95564fde6a9d5b85415a8d
divjazz03 <divjazz03@gmail.com>
2024-08-27 12:01:51 +0100

    Created the project base structure

---
+++ src/main/java/com/project/BRP_backend/BrpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.BRP_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/BRP_backend/BrpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.BRP_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
