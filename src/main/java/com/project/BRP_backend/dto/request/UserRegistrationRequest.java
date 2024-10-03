838fe2fc506d0cc8b18221ccfe6acc382a5480a9
divjazz03 <divjazz03@gmail.com>
2024-10-03 10:05:34 +0100

    I created the create and read user functionality

--- src/main/java/com/project/BRP_backend/dto/request/UserRegistrationRequest.java
+++ src/main/java/com/project/BRP_backend/dto/request/UserRegistrationRequest.java
@@ -1,2 +1,16 @@
-package com.project.BRP_backend.dto.request;public class UserRegistrationRequest {
+package com.project.BRP_backend.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String userType;
+    private final String phoneNumber;
+    private final String email;
+    private final String password;
+    private final String gender;
+    private final String address;
+
 }
